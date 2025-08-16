7b65eeed0c2dfad2b838c41742e34c5e2e7d89ad
Humberto <humber.arraya@gmail.com>
2025-08-16 18:33:17 -0300

    style: render new InfoSection component in landing page and styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './index.css';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Products from './components/Products/Products';
+import InfoSection from './components/InfoSection/InfoSection';
 
 export default function App () {
   return (
@@ -11,6 +12,7 @@ export default function App () {
       <main>
         <Hero />
         <Products />
+        <InfoSection />
       </main>
     </>
 
---
+++ src/components/InfoSection/InfoSection.jsx
@@ -0,0 +1,17 @@
+import css from './InfoSection.module.css';
+
+export default function InfoSection () {
+  return (
+    <section className={css.infoSection}>
+      <div className={css.contentInfoSection}>
+        <img src="/assets/images/illustration-stay-productive.png" alt="illustration-fylo" />
+        <div className={css.contentDescription}>
+          <h2>Stay productive, wherever you are</h2>
+          <p>Never let location be an issue when accessing your files. Fylo has you covered for all of your file storage needs.</p>
+          <p>Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.</p>
+          <a href="#">See how fylo works</a>
+        </div>
+      </div>
+    </section>
+  )
+}
