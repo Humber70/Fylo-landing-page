7d459b458c3e4659462e349c958d59e86854da15
Humberto <humber.arraya@gmail.com>
2025-08-31 20:34:04 -0300

    refact: add new icon and update color link

--- src/components/InfoSection/InfoSection.jsx
+++ src/components/InfoSection/InfoSection.jsx
@@ -9,7 +9,12 @@ export default function InfoSection () {
           <h2>Stay productive, wherever you are</h2>
           <p>Never let location be an issue when accessing your files. Fylo has you covered for all of your file storage needs.</p>
           <p>Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.</p>
-          <a href="#">See how Fylo works</a>
+          <div>
+            <a href="#">
+              See how Fylo works
+              <img className={css.iconLink} src="/assets/icons/icon-arrow.svg" alt="icon-arrow" />
+            </a>
+          </div>
         </div>
       </div>
     </section>
