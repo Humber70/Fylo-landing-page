d8a01523d346d5ce5b600271ad33cdfd4483e5da
Humberto <humber.arraya@gmail.com>
2025-09-19 16:26:46 -0300

    refact: add validation form and display message error

--- src/components/EmailSignup/EmailSignup.jsx
+++ src/components/EmailSignup/EmailSignup.jsx
@@ -1,6 +1,34 @@
 import css from './EmailSignup.module.css'
+import { useState } from 'react';
 
 export default function EmailSignup () {
+  
+  const [messageError, setMessageError] = useState({
+    state: null,
+    message: ''
+  });
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    // get value of input email 
+    let fieldEmail = new FormData(event.target).get('email')
+    
+    // mini Validation and set message error
+    if(!fieldEmail) return setMessageError({
+      state: true,
+      message: 'Please enter a valid email Address'
+    })
+    
+    // clear input and new message Successful
+    setMessageError({
+      state: false,
+      message: 'Thanks'
+    })
+    document.querySelector('#email').value = ''
+
+  }
+
   return (
     <section className={css.emailSignup}>
       <div className={css.contentEmailSignup}>
@@ -8,10 +36,12 @@ export default function EmailSignup () {
           <h2>Get early access today</h2>
           <p>It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions, our support team would be happy to help you.</p>
         </div>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input type="email" placeholder='email@example.com'/>
-          <button>Get Started For Free</button>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email"></label>
+          <input id="email" type="email" placeholder='email@example.com' name='email'/>
+          <button type='submit'>Get Started For Free</button>
         </form>
+        {<span className={messageError.state ? `${css.messageError}` : `${css.messageSuccessful}`}>{messageError.state ? messageError.message : messageError.message}</span>}
       </div>
     </section>
   )
