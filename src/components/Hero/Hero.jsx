8a7cd619a4a43e5ae15d54813a97afff495c1007
Humberto <humber.arraya@gmail.com>
2025-11-13 16:45:27 -0300

    refact: update button element in Hero component to include type attribute

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero () {
       <div className={css.heroContent}>
         <h1>All your files in one secure location, accessible anywhere.</h1>
         <p>Fylo stores all your most important files in one secure location. Access them wherever you need, share and collaborate with friends family, and co-workers.</p>
-        <button>Get Started</button>
+        <button type='button'>Get Started</button>
       </div>
     </section>
   )
